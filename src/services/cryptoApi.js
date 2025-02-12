08457c83f12f4bbe9ef111bd3a90edcbc65a4d13
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-12 17:43:45 +0330

    Displey Coins

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,4 +3,4 @@ const API_KEY="CG-pxJBexNRy7JFUBJgoEJRiQju"
 const getCoinList=()=>{
     return `${BASE_APT}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=${API_KEY}`
 }
-export{getCoinList};
+export default getCoinList;
--- src/templates/HomePage.jsx
+++ src/templates/HomePage.jsx
@@ -4,14 +4,14 @@ import getCoinList from '../services/cryptoApi';
 
 
 function HomePage() {
-    const[coins , setCoins]=useState([]);
+    const[coins,setCoins]=useState([]);
     useEffect(()=>{
         const getData=async()=>{
             const res=await fetch(getCoinList());
             const json=await res.json();
-            setCoins(json)
+            setCoins(json);
         }
-        getData();
+      getData();
     },[])
   return (
     <div>
