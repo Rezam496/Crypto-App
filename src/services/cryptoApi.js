9d8120d40cf0f9b3f12e6f0caaf6f57bc9c4eeeb
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-13 10:33:18 +0330

    Change currency related to data

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,6 +1,6 @@
 const BASE_APT="https://api.coingecko.com/api/v3";
 const API_KEY="CG-pxJBexNRy7JFUBJgoEJRiQju"
-const getCoinList=(page)=>{
-    return `${BASE_APT}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`
+const getCoinList=(page,currency)=>{
+    return `${BASE_APT}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`
 }
 export default getCoinList;
